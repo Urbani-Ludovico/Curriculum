8c6d553ff66d8d844959368330138c81d0485910
Ludovico-Urbani <lu.urbani@outlook.com>
2024-03-29 16:11:11 +0100

    Sport referee duties

---
+++ api/sport_referee.py
@@ -0,0 +1,29 @@
+from os import path
+
+from modules.date import Date, format_range
+from modules.db import get_cursor
+from modules.latex_blocks import timeline, timeline_content
+from modules.location import Location
+
+
+def main():
+    cursor = get_cursor(path.abspath("db/db.sqlite"))
+
+    cursor.execute("SELECT * FROM SPORT_REFEREE ORDER BY START DESC, COMPETITION")
+
+    print(
+        "\n".join(
+            [timeline(
+                date = format_range(Date(row["START"]), Date(row["END"])),
+                content = timeline_content(
+                    row["COMPETITION"],
+                    location = Location(cursor, row["LOCATION"]),
+                    content = row["LATEX_DESCRIPTION"]
+                )
+            ) for row in cursor.fetchall()]
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
